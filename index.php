38341d6e50d3c64fe9b5e06f04008c075c6b646a
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-17 13:44:56 +0200

    [UPD] added construct

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
         public $titolo;
         public $anno_uscita;
         public $genere;
+
+        public function __construct($_titolo, $_anno_uscita, $_genere) {
+            $this->titolo = $_titolo;
+            $this->anno_uscita = $_anno_uscita;
+            $this->genere = $_genere;
+        }
     }
 ?>
