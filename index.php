01decbf1d6c3dc8f23d2b1e46b70d5d58a2d1fad
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-17 14:00:33 +0200

    [UPD] bonus 1 done

--- index.php
+++ index.php
@@ -4,19 +4,20 @@
         public $anno_uscita;
         public $genere;
 
-        public function __construct($_titolo, $_anno_uscita, $_genere) {
+        public function __construct($_titolo, $_anno_uscita, $_genere = []) {
             $this->titolo = $_titolo;
             $this->anno_uscita = $_anno_uscita;
             $this->genere = $_genere;
         }
 
         public function getMovieInfo() {
-            return "Titolo: {$this->titolo}, Genere: {$this->genere}, Anno: {$this->anno_uscita}";
+            $lista_generi = implode(", ", $this->genere);
+            return "Titolo: {$this->titolo}, Generi: {$lista_generi}, Anno: {$this->anno_uscita}";
         }
     }
 
-    $movie1 = new Movie("Inception", 2010, "Science fiction");
-    $movie2 = new Movie("The Matrix", 1999, "Action");
+    $movie1 = new Movie("Inception", 2010, ["Science fiction", "Action", "Adventure", "Drama", "Supernatural", "Thriller", "Romance", "Suspense"]);
+    $movie2 = new Movie("The Matrix", 1999, ["Action", "Adventure", "Science fiction", "Fantasy", "Cyberpunk", "Noir", "Drama"]);
 
     echo $movie1->getMovieInfo();
     echo "<br>";
